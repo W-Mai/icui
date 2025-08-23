60ea2835ce0c8c809f6365568f4284df944d8e72
W-Mai <1341398182@qq.com>
2025-08-23 07:20:09 +0000

    Deploying to gh-pages from @ W-Mai/icu@af8001b373ce66fb1dc602ac174a0af0ede2d5f3 🚀

--- icu_tool-c6f7ed0a0004d850.js
+++ icu_tool-c6f7ed0a0004d850.js
@@ -198,7 +198,7 @@ function debugString(val) {
     return className;
 }
 function __wbg_adapter_30(arg0, arg1, arg2) {
-    wasm.closure82_externref_shim(arg0, arg1, arg2);
+    wasm.closure79_externref_shim(arg0, arg1, arg2);
 }
 
 function takeFromExternrefTable0(idx) {
@@ -207,7 +207,7 @@ function takeFromExternrefTable0(idx) {
     return value;
 }
 function __wbg_adapter_35(arg0, arg1) {
-    const ret = wasm._dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hedecb7c7ea8ef166_multivalue_shim(arg0, arg1);
+    const ret = wasm._dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hc785f0c7aff224ea_multivalue_shim(arg0, arg1);
     if (ret[1]) {
         throw takeFromExternrefTable0(ret[0]);
     }
@@ -1453,7 +1453,7 @@ function __wbg_get_imports() {
         const ret = makeMutClosure(arg0, arg1, 80, __wbg_adapter_30);
         return ret;
     };
-    imports.wbg.__wbindgen_closure_wrapper886 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper885 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 80, __wbg_adapter_35);
         return ret;
     };
